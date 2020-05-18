0c3cb4a8c50ff3e63b255b377c35544f92b70f6f
AlexanderBotnari <63365662+AlexanderBotnari@users.noreply.github.com>
2020-05-18 15:16:42 +0300

    Add files via upload

---
+++ Aplication.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+public class Aplication {
+	public static void main(String[] args) throws IOException {
+          
+		final int  VALID_PIN  = 1234;  
+	          int  scannedPin = 0;
+	       
+	       System.out.print("Enter your pin ! >>>");
+           int number1 = System.in.read();
+           int number2 = System.in.read();
+           int number3 = System.in.read();
+           int number4 = System.in.read();
+           
+           scannedPin = (Character.getNumericValue(number1)*1000) + (Character.getNumericValue(number2)*100) 
+        		   + (Character.getNumericValue(number3)*10) + Character.getNumericValue(number4);
+          
+           if(VALID_PIN == scannedPin) {
+        	   System.out.println("True ! Account accesible !");
+           }else {
+        	   System.err.println("False ! Incorect pin !");
+           }
+    }
+}
